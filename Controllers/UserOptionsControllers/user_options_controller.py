826db12e6908b478d3525f2424c0ffa7d62e57b6
VladMikheda <vladmikheda@gmail.com>
2023-08-29 22:21:20 +0200

    add(Controllers/UserOptionsControllers): add controllers for user options menu and user add menu

---
+++ Controllers/UserOptionsControllers/user_options_controller.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+
+from Controllers.UserOptionsControllers.help_functions import delete_message
+from Utils.FSM.UserOptions import UserOptionsFSM
+from Utils.Keyboards.Inline.UserOptions import (get_user_options_menu, get_user_add_menu)
+
+
+async def open_user_option_menu_controller(message: Message, state: FSMContext):
+    await state.set_state(UserOptionsFSM.state_user_options_menu_open)
+    asyncio.create_task(delete_message(message))
+    await message.answer('User options', reply_markup=get_user_options_menu())
+
+
+async def close_user_option_menu_controller(call: CallbackQuery, state: FSMContext):
+    await state.clear()
+    await call.answer()
+    await call.message.delete()
+
+
+async def open_user_add_menu_controller(call: CallbackQuery, state: FSMContext):
+    await state.set_state(UserOptionsFSM.state_open_add_user_menu)
+    await call.answer()
+    await call.message.delete()
+    await call.message.answer('Add user menu', reply_markup=get_user_add_menu())
--- Controllers/__init__.py
+++ Controllers/__init__.py
@@ -1,1 +1,11 @@
 from .command_controller import start_controller, help_controller, stop_controller
+from Controllers.UserOptionsControllers.user_options_controller import (open_user_option_menu_controller,
+                                                                        close_user_option_menu_controller,
+                                                                        open_user_add_menu_controller, )
+
+from Controllers.UserOptionsControllers.user_add_controller import (get_wait_user_id_message_controller,
+                                                                    get_wait_user_name_message_controller,
+                                                                    get_wait_user_default_payment_message_controller,
+                                                                    write_new_user_id_controller,
+                                                                    write_new_user_name_controller,
+write_new_user_default_payment_controller, add_new_user_controller)
