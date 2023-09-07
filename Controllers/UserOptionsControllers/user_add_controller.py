736d2c32408238200353d9f6aac6fdeda2b38c3e
VladMikheda <vladmikheda@gmail.com>
2023-09-07 09:26:40 +0200

    refactor(user_add_controller.py): refactoring cancel and write user controller for better performance

--- Controllers/UserOptionsControllers/user_add_controller.py
+++ Controllers/UserOptionsControllers/user_add_controller.py
@@ -4,22 +4,18 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 
 from Controllers.UserOptionsControllers.help_functions import delete_message, send_submenu_to_callback, \
-    delete_menu_message, incorrect_user_argument, write_new_user_argument, back_to_user_options_menu, \
+    delete_menu_message, incorrect_user_argument, write_new_user_argument, \
     send_cancel_menu_to_callback, get_user_id
 from Models.User import User
 from Utils.FSM.UserOptions.UserOptionsFSM import UserOptionsFSM
 from Utils.Keyboards.Inline.UserOptions.user_options_keyboards import get_user_options_menu, get_user_add_menu
 
 
-async def add_new_user_main_cancel_controller(call: CallbackQuery, state: FSMContext):
-    await back_to_user_options_menu(call, state)
-
-
-async def add_new_user_cancel_controller(call: CallbackQuery, state: FSMContext):
-    message_text = 'Add user menu'
-    keyboard = get_user_add_menu()
-    await state.set_state(UserOptionsFSM.state_open_add_user_menu)
-    await send_submenu_to_callback(message_text, keyboard, state, call)
+async def add_new_user_main_cancel_controller(call: CallbackQuery, state: FSMContext) -> Message:
+    await call.answer()
+    await state.clear()
+    message = call.message
+    return message
 
 
 async def get_wait_user_id_message_controller(call: CallbackQuery, state: FSMContext):
@@ -47,7 +43,7 @@ async def write_new_user_id_controller(message: Message, state: FSMContext):
     asyncio.create_task(delete_menu_message(state))
 
     # check message is contact or id number
-    user_id, error_message = get_user_id(error_message, message)
+    user_id, error_message = await get_user_id(error_message, message)
 
     #  if message is not correct return error message
     if user_id is None:
@@ -132,6 +128,10 @@ async def add_new_user_controller(call: CallbackQuery, state: FSMContext):
         message_answer = await call.message.answer(error_message)
         asyncio.create_task(delete_message(message_answer, 10))
     finally:
-        await state.set_state(UserOptionsFSM.state_user_options_menu_open)
-        await call.message.answer('User options', reply_markup=get_user_options_menu())
-        asyncio.create_task(delete_message(call.message))
+        await call.answer()
+        await state.clear()
+        message = call.message
+        return message
+        # await state.set_state(UserOptionsFSM.state_user_options_menu_open)
+        # await call.message.answer('User options', reply_markup=get_user_options_menu())
+        # asyncio.create_task(delete_message(call.message))
